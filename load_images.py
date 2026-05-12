281266ac6ffb9a52c1f89199a4a66d4b6a7b52eb
lotus64yt <lotus64santon@gmail.com>
2026-05-12 10:02:23 +0200

    feat: reorder image loading

--- load_images.py
+++ load_images.py
@@ -4,8 +4,11 @@ import os
 def loadImages(path):
     images = {}
     filesInFolder = os.listdir(path)
-    for file in filesInFolder:
-        if file.endswith('.png'):
+    print(filesInFolder)
+    desired_order = ['Capture.png', 'Capture2.png', 'Capture3.png', 'Capture4.png', 'Capture5.png', 'Capture6.png', 'Capture7.png', 'Capture8.png', 'Capture9.png', 'Capture10.png']
+    files_set = set(filesInFolder)
+    for file in desired_order:
+        if file in files_set and file.endswith('.png'):
             imageName = os.path.splitext(file)[0]
             imagePath = os.path.join(path, file)
             images[imageName] = pygame.image.load(imagePath).convert_alpha()
